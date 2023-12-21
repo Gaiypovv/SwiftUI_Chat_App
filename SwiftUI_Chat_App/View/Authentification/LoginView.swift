f68dd3d018294173aaa00d7c929c8e00a61bdca9
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-12-22 00:31:09 +0600

    implemented the email textfield and password securefield

--- SwiftUI_Chat_App/View/Authentification/LoginView.swift
+++ SwiftUI_Chat_App/View/Authentification/LoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
@@ -21,6 +24,13 @@ struct LoginView: View {
                     .font(.largeTitle)
                     .bold()
                     .foregroundColor(.blue)
+                
+                VStack(spacing: 20) {
+                    TextField("Email", text: $email)
+                    
+                    SecureField("Password", text: $password)
+                }
+                .padding([.top, .horizontal], 32)
                 
                 Spacer()
             }
